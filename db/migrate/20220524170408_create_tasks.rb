fd7f7d9f811ef8f87c54c200ea6750d7e9a7ace9
ryllerpadua <ryllerpadua@yahoo.com.br>
2022-05-24 18:20:17 -0300

    task complete

--- db/migrate/20220524170408_create_tasks.rb
+++ db/migrate/20220524170408_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration[6.1]
   def change
     create_table :tasks do |t|
       t.string :title
-      t.texts :details
+      t.text :details
       t.boolean :completed, default: false
 
       t.timestamps
